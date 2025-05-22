d9e51ea12a53a61b1cb8322def0a9fe7d0df7b85
alexnec29 <alexandrunec29@gmail.com>
2025-05-22 16:37:04 +0300

    Add RSS Section Statistics

--- src/fizio/statistics-fizio.php
+++ src/fizio/statistics-fizio.php
@@ -25,6 +25,12 @@
             <li>Reeducare membre inferioare: 3 sesiuni</li>
         </ul>
     </div>
+
+    <div class="rss-section">
+        <h3>Fluxul tău RSS personal</h3>
+        <p>Pentru a primi actualizări automate cu progresul tău, poți folosi acest link RSS în orice cititor de feed-uri:</p>
+        <a href="rss-kineto.php" class="rss-link" target="_blank">📥 Vezi fluxul RSS</a>
+    </div>
 </body>
 
 </html>
