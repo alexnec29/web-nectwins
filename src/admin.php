0b019d0066122a3c34a67891989fe55ff088f5b8
alexnec29 <alexandrunec29@gmail.com>
2025-06-14 22:53:15 +0300

    Solve back button

--- src/admin.php
+++ src/admin.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION["username"]) || !isset($_SESSION["role"]) || $_SESSION["rol
 
     <nav>
         <h1>Panou Administrativ</h1>
-        <a href="/gym/principal-gym.php" class="nav-link">Înapoi</a>
+        <a href="principal.php" class="nav-link">Înapoi</a>
     </nav>
 
     <main class="admin-dashboard">
