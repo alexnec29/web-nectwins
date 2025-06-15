a8b88aec48a8dad82294c08e065602eb522ce808
alexnec29 <alexandrunec29@gmail.com>
2025-06-16 00:06:45 +0300

    REpair PDF export - path vendor

--- src/optiuni/leaderboard.pdf.php
+++ src/optiuni/leaderboard.pdf.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION["user_id"])) {
     exit;
 }
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 // 🔴 Linia de mai jos NU trebuie
 // use TCPDF;
 
